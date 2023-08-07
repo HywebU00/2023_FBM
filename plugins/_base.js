3f404bc07bd7c4d1bb9708fcb69a11ce3544e4c9
Tseng <tseng7510@gmail.com>
2023-08-07 14:49:00 +0800

    更新

--- plugins/_base.js
+++ plugins/_base.js
@@ -1,14 +1,38 @@
 // 開啟畫面動態效果
-new WOW({
-  boxClass: 'wow', // default
-  animateClass: 'animated', // default
-  offset: 50, // default
-  mobile: true, // default
-  live: true, // default
-}).init();
+// new WOW({
+//   boxClass: 'wow', // default
+//   animateClass: 'animated', // default
+//   offset: 50, // default
+//   mobile: true, // default
+//   live: true, // default
+// }).init();
 
-const menuNextLv = document.querySelectorAll('nav ul ul');
+// const menuNextLv = document.querySelectorAll('nav ul ul');
 
-menuNextLv.forEach((item) => {
-  item.parentElement.classList.add('next-lv');
-});
+// menuNextLv.forEach((item) => {
+//   item.parentElement.classList.add('next-lv');
+// });
+
+(function () {
+  let options = {};
+  // let removeAnimate = document.querySelector('.removeAnimate');
+
+  let callback = (entries, observer) => {
+    entries.forEach((item, index) => {
+      if (item.isIntersecting) {
+        item.target.classList.add('animated');
+        // setTimeout(() => {
+        //   removeAnimate.classList.remove('animated');
+        // }, 1000);
+        // } else {
+      }
+    });
+  };
+  let observer = new IntersectionObserver(callback, options);
+  const yoo = document.querySelectorAll('.yoo');
+  yoo.forEach((item) => {
+    const delay = item.dataset.yooDelay;
+    item.style.animationDelay = delay;
+    observer.observe(item);
+  });
+})();
